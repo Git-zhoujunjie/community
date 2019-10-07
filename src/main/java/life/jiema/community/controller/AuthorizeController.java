67122a215b55a1dc5f6d0b531de5a9d4aee36882
Git-zhoujunjie <1160529743@qq.com>
2019-10-07 20:13:59 +0800

    show the username after login in

--- src/main/java/life/jiema/community/controller/AuthorizeController.java
+++ src/main/java/life/jiema/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Zhoujunjie
  * @create 2019/9/24 16:21
@@ -27,7 +29,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -37,6 +40,14 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = githubProvider.getUser(accessToken);
 //        System.out.println(user.getName());
+        if(user!=null){
+            // 登录成功，设置cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+
+        } else {
+            // 重新登录
+        }
         return "index";
     }
 }
