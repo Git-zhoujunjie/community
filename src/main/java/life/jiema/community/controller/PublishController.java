961f54ef806a5cb0ec5eb565048563e8b868bfde
Git-zhoujunjie <1160529743@qq.com>
2019-10-09 00:19:18 +0800

    create table question

--- src/main/java/life/jiema/community/controller/PublishController.java
+++ src/main/java/life/jiema/community/controller/PublishController.java
@@ -1,7 +1,18 @@
 package life.jiema.community.controller;
 
+import life.jiema.community.dto.Question;
+import life.jiema.community.dto.User;
+import life.jiema.community.mapper.QuestionMapper;
+import life.jiema.community.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Zhoujunjie
@@ -10,8 +21,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublishController {
 
+    @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model
+    ){
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+            }
+            break;
+        }
+
+        if(user==null){
+            model.addAttribute("error","用户未登录！");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(user.getGmtModified());
+        questionMapper.create(question);
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/life/jiema/community/mapper/QuestionMapper.java
@@ -0,0 +1,16 @@
+package life.jiema.community.mapper;
+
+import life.jiema.community.dto.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Zhoujunjie
+ * @create 2019/10/8 14:46
+ */
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    int create(Question question);
+}
